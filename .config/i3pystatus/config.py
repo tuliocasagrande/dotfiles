db4cb844414ea901b2e8c6dc059394adaad7ef15
Tulio Casagrande <tuliocasagrande@gmail.com>
2018-01-25 09:55:59 -0200

    Improve i3 experience

--- .config/i3pystatus/config.py
+++ .config/i3pystatus/config.py
@@ -6,21 +6,30 @@ status = Status()
 # Tue 30 Jul 11:59
 status.register(
     "clock",
-    format="%a %-d %b %Y %H:%M",)
+    format="%a %-d %b %Y %H:%M",
+    on_leftclick="gnome-calendar")
 
-status.register("cpu_usage_graph")
+status.register(
+    "cpu_usage_graph",
+    on_leftclick="gnome-system-monitor")
 
 status.register(
     "cpu_usage_bar",
     format="{usage_bar_cpu0}{usage_bar_cpu1}{usage_bar_cpu2}{usage_bar_cpu3}",
-    bar_type="vertical")
+    bar_type="vertical",
+    hints={"separator": False, "separator_block_width": 5},
+    on_leftclick="gnome-system-monitor")
 
 # Shows your CPU temperature, if you have a Intel CPU
 status.register(
     "temp",
-    format="{temp:.0f}°C",)
+    lm_sensors_enabled=True,
+    format="{temp:.0f}°C",
+    on_leftclick="gnome-system-monitor")
 
-status.register("mem_bar")
+status.register(
+    "mem_bar",
+    on_leftclick="gnome-system-monitor")
 
 # The battery monitor has many formatting options, see README for details
 
@@ -35,7 +44,7 @@ status.register("mem_bar")
 #   http://www.knopwob.org/dunst/
 status.register(
     'battery',
-    format='Battery [{status} ]{percentage_design:.0f}% {remaining}',
+    format='Battery [{status} ]{percentage_design:.0f}%[ {remaining}]',
     alert=True,
     alert_percentage=15,
     alert_format_body="Battery has only {percentage:.0f}% ({remaining}) "
@@ -58,26 +67,27 @@ status.register(
 status.register(
     "network",
     interface="enp3s0",
-    format_up="{bytes_sent:3.0f}🢁 {bytes_recv:4.0f}🢃 {network_graph_recv}",
+    format_up="{bytes_recv} KB/s {network_graph_recv}",
     format_down="")
 
 # Note: requires both netifaces and basiciw (for essid and quality)
 status.register(
     "network",
     interface="wlp2s0",
-    format_up="{essid} ({quality}%): {bytes_sent:3.0f}🢁 {bytes_recv:4.0f}🢃 "
-              "{network_graph_recv}",
-    format_down="wifi: down"),
+    format_up="{essid} ({quality}%) {bytes_recv}KB/s {network_graph_recv}",
+    recv_limit=6144)
 
 # Shows disk usage of / and /home
 status.register(
     "disk",
     path="/home",
-    format="/home: {avail:.1f}G",)
+    format="/home: {avail:.1f}G",
+    on_leftclick="nautilus")
 status.register(
     "disk",
     path="/",
-    format="/: {avail:.1f}G",)
+    format="/: {avail:.1f}G",
+    on_leftclick="nautilus")
 
 # Shows pulseaudio default sink volume
 #
