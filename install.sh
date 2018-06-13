ea90058a9f64d49867ed19ef7c5dfe470d8e5ccc
Tulio Casagrande <tuliocasagrande@gmail.com>
2018-06-13 13:28:06 +0200

    Add i3 to installation file

--- install.sh
+++ install.sh
@@ -5,7 +5,11 @@ set -e
 ln -s ~/dotfiles/.gitconfig ~/
 ln -s ~/dotfiles/.vimrc ~/
 ln -s ~/dotfiles/.tmux.conf ~/
-ln -s ~/dotfiles/.jupyter/jupyter_notebook_config.py .jupyter/
+ln -s ~/dotfiles/.jupyter/jupyter_notebook_config.py ~/.jupyter/
+
+mkdir ~/.config/i3/ ~/.config/i3pystatus/
+ln -s ~/dotfiles/.config/i3/config ~/.config/i3/
+ln -s ~/dotfiles/.config/i3pystatus/config.py ~/.config/i3pystatus/
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
