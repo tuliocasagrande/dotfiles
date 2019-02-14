b83e2613687035e6a3364c31aab7db74fbaaa776
Tulio Casagrande <tuliocasagrande@gmail.com>
2019-02-14 12:41:31 -0200

    Remove bash-git-prompt

--- install.sh
+++ install.sh
@@ -6,13 +6,9 @@ ln -s ~/dotfiles/.gitconfig ~/
 ln -s ~/dotfiles/.vimrc ~/
 ln -s ~/dotfiles/.tmux.conf ~/
 ln -s ~/dotfiles/.inputrc ~/
-ln -s ~/dotfiles/.bashrc ~/
-ln -s ~/dotfiles/.git-prompt-colors-sh ~/
 
 mkdir ~/.jupyter/
 ln -s ~/dotfiles/.jupyter/jupyter_notebook_config.py ~/.jupyter/
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
-
-git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt
