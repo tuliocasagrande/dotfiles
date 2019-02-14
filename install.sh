a3ecb04a722c82dec6ffe8e9a662060187411114
Tulio Casagrande <tuliocasagrande@gmail.com>
2019-02-14 12:34:07 -0200

    Remove i3 scripts

--- install.sh
+++ install.sh
@@ -12,10 +12,6 @@ ln -s ~/dotfiles/.git-prompt-colors-sh ~/
 mkdir ~/.jupyter/
 ln -s ~/dotfiles/.jupyter/jupyter_notebook_config.py ~/.jupyter/
 
-mkdir ~/.config/i3/ ~/.config/i3pystatus/
-ln -s ~/dotfiles/.config/i3/config ~/.config/i3/
-ln -s ~/dotfiles/.config/i3pystatus/config.py ~/.config/i3pystatus/
-
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
