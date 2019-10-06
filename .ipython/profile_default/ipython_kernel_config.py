88da448ce3b158fb0edde24770fffeae1d563e94
Tulio Casagrande <tuliocasagrande@gmail.com>
2019-10-06 12:10:18 -0300

    Enable high resolution images (pixel-doubled PNG)

---
+++ .ipython/profile_default/ipython_kernel_config.py
@@ -0,0 +1,2 @@
+# Enable high resolution images (pixel-doubled PNG)
+c.InlineBackend.figure_format = 'retina'
--- install.sh
+++ install.sh
@@ -10,5 +10,8 @@ ln -s ~/dotfiles/.inputrc ~/
 mkdir ~/.jupyter/
 ln -s ~/dotfiles/.jupyter/jupyter_notebook_config.py ~/.jupyter/
 
+mkdir -p ~/.ipython/profile_default/
+ln -s ~/dotfiles/.ipython/profile_default/ipython_kernel_config.py ~/.ipython/profile_default/
+
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
