95d25437013286b895273d3128cb16ff97479f5c
Tulio Casagrande <tuliocasagrande@gmail.com>
2019-11-24 11:25:47 -0300

    Add jupyter lab shortcuts

--- install.sh
+++ install.sh
@@ -10,6 +10,9 @@ ln -s ~/dotfiles/.inputrc ~/
 mkdir ~/.jupyter/
 ln -s ~/dotfiles/.jupyter/jupyter_notebook_config.py ~/.jupyter/
 
+mkdir ~/.jupyter/lab/user-settings/@jupyterlab/shortcuts-extension/
+ln -s ~/dotfiles/.jupyter/shortcuts.jupyterlab-settings ~/.jupyter/lab/user-settings/@jupyterlab/shortcuts-extension/
+
 mkdir -p ~/.ipython/profile_default/
 ln -s ~/dotfiles/.ipython/profile_default/ipython_kernel_config.py ~/.ipython/profile_default/
 
