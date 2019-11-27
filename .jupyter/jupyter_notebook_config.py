fe9bb76c9a2d1756fa536a0d9cfe010f38b6e6d3
Tulio Casagrande <tuliocasagrande@gmail.com>
2019-11-27 09:58:39 -0300

    Trim whitespaces on save

--- .jupyter/jupyter_notebook_config.py
+++ .jupyter/jupyter_notebook_config.py
@@ -1,11 +1,13 @@
-# If you want to auto-save .html and .py versions of your notebook:
-# modified from: https://github.com/ipython/ipython/issues/8009
 import os
 from subprocess import check_call
 
 
+# -----------------------------------------------------------------------------
+# Auto-save .py and .html
+# modified from: https://github.com/ipython/ipython/issues/8009
+# -----------------------------------------------------------------------------
 def post_save(model, os_path, contents_manager):
-    """post-save hook for converting notebooks to .py and .html"""
+    """Post-save hook for converting notebooks to .py and .html"""
     if model['type'] != 'notebook' or model['name'].startswith('Untitled'):
         return  # only do this for notebooks
     d, fname = os.path.split(os_path)
@@ -14,3 +16,32 @@ def post_save(model, os_path, contents_manager):
 
 
 c.FileContentsManager.post_save_hook = post_save
+
+
+# -----------------------------------------------------------------------------
+# Strip trailing whitespace
+# https://github.com/jupyter/notebook/issues/1455
+# -----------------------------------------------------------------------------
+def strip_whitespace(text):
+    return '\n'.join([line.rstrip() for line in text.split('\n')])
+
+
+def strip_whitespace_pre_save(model=None, **kwargs):
+    """Pre-save hook for stripping trailing whitespace."""
+    if model['type'] == 'notebook':
+        # only run on nbformat v4
+        if model['content']['nbformat'] != 4:
+            print("Skipping whitespace stripping since `nbformat` != 4")
+            return
+        print("Stripping whitespace")
+        for cell in model['content']['cells']:
+            if cell['cell_type'] != 'code':
+                continue
+            cell['source'] = strip_whitespace(cell['source'])
+    elif model['type'] == 'file':
+        if model['format'] == 'text':
+            print("Stripping whitespace")
+            model['content'] = strip_whitespace(model['content'])
+
+
+c.ContentsManager.pre_save_hook = strip_whitespace_pre_save
