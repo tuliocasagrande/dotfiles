27823afa5a5d205291502ff55841df3a22351b53
Tulio Casagrande <tuliocasagrande@gmail.com>
2024-08-07 16:31:11 -0700

    Add new jupyter config files

--- .jupyter/jupyter_notebook_config.py
+++ .jupyter/jupyter_notebook_config.py
@@ -8,13 +8,11 @@ from subprocess import check_call
 # -----------------------------------------------------------------------------
 def post_save(model, os_path, contents_manager):
     """Post-save hook for converting notebooks to .py and .html"""
-    if model['type'] != 'notebook' or model['name'].startswith('Untitled'):
+    if model["type"] != "notebook" or model["name"].startswith("Untitled"):
         return  # only do this for notebooks
     d, fname = os.path.split(os_path)
-    check_call(['jupyter', 'nbconvert', '--output-dir', 'exports/', '--to',
-                'script', fname], cwd=d)
-    check_call(['jupyter', 'nbconvert', '--output-dir', 'exports/', '--to',
-                'html', fname], cwd=d)
+    check_call(["jupyter", "nbconvert", "--output-dir", "exports/", "--to", "script", fname], cwd=d)
+    check_call(["jupyter", "nbconvert", "--output-dir", "exports/", "--to", "html", fname], cwd=d)
 
 
 c.FileContentsManager.post_save_hook = post_save
@@ -24,26 +22,26 @@ c.FileContentsManager.post_save_hook = post_save
 # Strip trailing whitespace
 # https://github.com/jupyter/notebook/issues/1455
 # -----------------------------------------------------------------------------
-def strip_whitespace(text):
-    return '\n'.join([line.rstrip() for line in text.split('\n')])
-
-
 def strip_whitespace_pre_save(model=None, **kwargs):
     """Pre-save hook for stripping trailing whitespace."""
-    if model['type'] == 'notebook':
+
+    def strip_whitespace(text):
+        return "\n".join([line.rstrip() for line in text.split("\n")])
+
+    if model["type"] == "notebook":
         # only run on nbformat v4
-        if model['content']['nbformat'] != 4:
+        if model["content"]["nbformat"] != 4:
             print("Skipping whitespace stripping since `nbformat` != 4")
             return
         print("Stripping whitespace")
-        for cell in model['content']['cells']:
-            if cell['cell_type'] != 'code':
+        for cell in model["content"]["cells"]:
+            if cell["cell_type"] != "code":
                 continue
-            cell['source'] = strip_whitespace(cell['source'])
-    elif model['type'] == 'file':
-        if model['format'] == 'text':
+            cell["source"] = strip_whitespace(cell["source"])
+    elif model["type"] == "file":
+        if model["format"] == "text":
             print("Stripping whitespace")
-            model['content'] = strip_whitespace(model['content'])
+            model["content"] = strip_whitespace(model["content"])
 
 
 c.ContentsManager.pre_save_hook = strip_whitespace_pre_save
--- install.sh
+++ install.sh
@@ -1,24 +1,66 @@
 #!/usr/bin/env bash
 
+##### MISC #####
 ln -s ~/dotfiles/.gitconfig ~/
 ln -s ~/dotfiles/.vimrc ~/
 ln -s ~/dotfiles/.tmux.conf ~/
 ln -s ~/dotfiles/.inputrc ~/
 
-mkdir -p ~/.config/fish/functions/
-ln -s ~/dotfiles/.config/fish/fish_variables ~/.config/fish/fish_variables
-ln -s ~/dotfiles/.config/fish/functions/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
-ln -s ~/dotfiles/.config/fish/functions/rm_dsstore.fish ~/.config/fish/functions/rm_dsstore.fish
-ln -s ~/dotfiles/.config/fish/functions/mkcd.fish ~/.config/fish/functions/mkcd.fish
+##### FISH #####
+FISH_DIR=~/.config/fish
+mkdir -p ~/$FISH_DIR/functions/
+ln -s ~/dotfiles/$FISH_DIR/fish_variables ~/$FISH_DIR/fish_variables
+ln -s ~/dotfiles/$FISH_DIR/functions/fish_prompt.fish ~/$FISH_DIR/functions/fish_prompt.fish
+ln -s ~/dotfiles/$FISH_DIR/functions/rm_dsstore.fish ~/$FISH_DIR/functions/rm_dsstore.fish
+ln -s ~/dotfiles/$FISH_DIR/functions/mkcd.fish ~/$FISH_DIR/functions/mkcd.fish
 
-mkdir ~/.jupyter/
-ln -s ~/dotfiles/.jupyter/jupyter_notebook_config.py ~/.jupyter/
+##### JUPYTER #####
 
-mkdir -p ~/.jupyter/lab/user-settings/@jupyterlab/shortcuts-extension/
-ln -s ~/dotfiles/.jupyter/shortcuts.jupyterlab-settings ~/.jupyter/lab/user-settings/@jupyterlab/shortcuts-extension/
+# Jupyter auto-save
+CONFIG_DIR=.jupyter
+CONFIG_FILE=jupyter_notebook_config.py
+mkdir -p ~/$CONFIG_DIR
+ln -s ~/dotfiles/$CONFIG_DIR/$CONFIG_FILE ~/$CONFIG_DIR
 
-mkdir -p ~/.ipython/profile_default/
-ln -s ~/dotfiles/.ipython/profile_default/ipython_kernel_config.py ~/.ipython/profile_default/
+# Jupyter LSP
+CONFIG_DIR=.jupyter/lab/user-settings/@jupyter-lsp/jupyterlab-lsp
+CONFIG_FILE=completion.jupyterlab-settings
+mkdir -p ~/$CONFIG_DIR
+ln -s ~/dotfiles/$CONFIG_DIR/$CONFIG_FILE ~/$CONFIG_DIR
+CONFIG_FILE=plugin.jupyterlab-settings
+ln -s ~/dotfiles/$CONFIG_DIR/$CONFIG_FILE ~/$CONFIG_DIR
 
+# Jupyter file editor
+CONFIG_DIR=.jupyter/lab/user-settings/@jupyterlab/fileeditor-extension
+CONFIG_FILE=plugin.jupyterlab-settings
+mkdir -p ~/$CONFIG_DIR
+ln -s ~/dotfiles/$CONFIG_DIR/$CONFIG_FILE ~/$CONFIG_DIR
+
+# Jupyter notebook editor
+CONFIG_DIR=.jupyter/lab/user-settings/@jupyterlab/notebook-extension
+CONFIG_FILE=tracker.jupyterlab-settings
+mkdir -p ~/$CONFIG_DIR
+ln -s ~/dotfiles/$CONFIG_DIR/$CONFIG_FILE ~/$CONFIG_DIR
+
+# Jupyter shortcuts
+CONFIG_DIR=.jupyter/lab/user-settings/@jupyterlab/shortcuts-extension
+CONFIG_FILE=shortcuts.jupyterlab-settings
+mkdir -p ~/$CONFIG_DIR
+ln -s ~/dotfiles/$CONFIG_DIR/$CONFIG_FILE ~/$CONFIG_DIR
+
+# Jupyter code formatter extension
+CONFIG_DIR=.jupyter/lab/user-settings/jupyterlab_code_formatter
+CONFIG_FILE=settings.jupyterlab-settings
+mkdir -p ~/$CONFIG_DIR
+ln -s ~/dotfiles/$CONFIG_DIR/$CONFIG_FILE ~/$CONFIG_DIR
+
+##### IPYTHON #####
+# Configure matplotlib to use high resolution images, may be obsolete as of 2024
+CONFIG_DIR=.ipython/profile_default
+CONFIG_FILE=ipython_kernel_config.py
+mkdir -p ~/$CONFIG_DIR/
+ln -s ~/dotfiles/$CONFIG_DIR/$CONFIG_FILE ~/$CONFIG_DIR
+
+##### VIM #####
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
